2b85c00f0193bcecb39bbba94dc621de18643148
Juliet <chepngetichjuliet@gmail.com>
2019-04-07 12:18:59 +0300

    deleted styles2.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,32 @@
-//User interface
+$(document).ready(function () {
 
-function Player() {
-    this.name="";
-    this.totalScore = 0;
-    this.cummulativeScore = 0;
+
+    //Hides Game rules
+    $("#rules").hide();
+
+    //Show the rules onclick
+    $(".well h3").click(function () {
+        $("#rules").fadeToggle(100);
+    });
+
+    //hides game section
+    $("#jump").hide();
+
+    //show game section onclick
+    $("#game").click(function () {
+        $("#jump").fadeToggle(100);
+        $("#tron").hide();
+    });
+});
+
+function roll() {
+    var rol = Math.ceil((Math.random() * 6));
+    $("#score").html(rol);
 }
 
-//roll method
-Player.prototype.roll = function () {
-    var rolledDice = Math.floor((Math.random() * 6) + 1);
-    if (rolledDice > 1) {
-        this.cummulativeScore+=rolledDice;
-    }
-    else {
-        this.cummulativeScore = 0;
-    }
-    return rolledDice;
-}
-//hold method
-Player.prototype.hold = function () {
-    this.totalScore += this.cummulativeScore;
-    this.cummulativeScore = 0;
-    return this.totalScore;
-}
-var playerOne = new Player();
-var playerTwo = new Player();
-var winscore = 0;
-
-
-var startGame = function (playerOneName, playerTwoName) {
-    playerOne.name = playerOneName;
-    playerTwo.name = playerTwoName;
-    return 0;
-}
-
-
-
-$("#p1-roll-button").click(
-    function (event) {
-        var rolledDice = playerOne.roll();
-        $("rolled").text("You rolled:" + rolledDice);
-        $("cummulative-score").text("Your total score:" + playerOne.cummulativeScore);
-        if (rolledDice == 1) {
-            return $("hold-button").trigger("click");
-        }
-        if ((playerOne.cummulativeScore + playerOne.totalScore) >= winscore) {
-            playerOne.totalScore = playerOne.cummulativeScore + playerOne.totalScore;
-            return winGame(playerOne);
-        }
-        return 0;
-    }
-)
+$("#roll").click(function () {
+    roll();
+});
 
 
 
